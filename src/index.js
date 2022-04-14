2d832b9cd2955b93a23c69f5848ca9819152df86
RodolfoArauj0 <arujo.ro@hotmail.com>
2022-04-13 21:47:19 -0300

    Adicionado use Cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const cors = require('cors');
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
 app.listen(port);
